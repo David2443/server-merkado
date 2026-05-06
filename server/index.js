83cd161583a8b2e9d017f2f192734310aa724dd8
David2443 <babnun211@gmail.com>
2026-05-06 10:48:28 +0300

    gata

--- server/index.js
+++ server/index.js
@@ -69,8 +69,8 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json({ limit: '5mb' })); 
-app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(hpp());
 
 // 🧼 Filtre de curățenie pentru formulare
